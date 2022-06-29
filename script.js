6fc108d3c5fdc6e284a6b0cfc4302c9f15ab740e
vincaf <vincenzocafassog@gmail.com>
2022-06-29 16:52:11 +0200

    dynamically added slide from js

---
+++ script.js
@@ -0,0 +1,55 @@
+const images = [
+    {
+        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        title: 'Svezia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Perù',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+        title: 'Chile',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Argentina',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        title: 'Colombia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+];
+
+const imgWrapper = document.getElementById('wrapper');
+
+for (let i = 0; i < images.length - 1; i++) {
+
+    const newImg = document.createElement('img');
+    const newTitle = document.createElement('h1');
+    const newSubtitle = document.createElement('p');
+
+    // aggiunta del contenuto nell'attributo
+    newImg.setAttribute('src', images.url[i]);
+    newTitle.setAttribute('src', images.title[i]);
+    newSubtitle.setAttribute('src', images.description[i]);
+
+    if(i===0){
+        newImg.classList.add('ms_active');
+        newTitle.classList.add('ms_active');
+        newSubtitle.classList.add('ms_active');
+    }
+
+    // scrittura in pagina
+    imgWrapper.append(newImg);
+    imgWrapper.append(newTitle);
+    imgWrapper.append(newSubtitle);
+}
+
